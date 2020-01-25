c629c65c99b86e6cb259a46aee121b1d15013363
Saswat Levin <saswatlevin@gmail.com>
2020-01-25 20:07:10 +0530

    ADDED RNB's CUDAEncrypt.py

--- CUDAEncrypt.py
+++ CUDAEncrypt.py
@@ -30,53 +30,6 @@ def HistEQ(img_in):
         cv2.imshow('Histogram equalized', img_out)
     return img_out
 
-def sha2Hash(filename):
-    hashobj = hashlib.sha256()
-    with open(filename,'rb') as f:
-        while True:
-            block = f.read(CONFIG.BUFF_SIZE)
-            if not block:
-                break
-            hashobj.update(block)
-    return int(hashobj.hexdigest(),16)
-
-# Mersenne-Twister Intra-Column-Shuffle
-def MTShuffle(img_in, imghash):
-    mask = 2**CONFIG.MASK_BITS - 1   # Default: 8 bits
-    temphash = imghash
-    dim = img_in.shape
-    N = dim[0]
-    img_out = img_in.copy()
-
-    for j in range(N):
-        random.seed(temphash & mask)
-        MTmap = list(range(N))
-        random.shuffle(MTmap)
-        temphash = temphash>>CONFIG.MASK_BITS
-        if temphash==0:
-            temphash = imghash
-        for i in range(N):
-            index = int(MTmap[i])
-            img_out[i][j] = img_in[index][j]
-    return img_out
-
-#XOR Image with a Fractal
-def FracXor(imghash):
-    #Open the image
-    img_in = cv2.imread("4mtshuffle.png", 1)
-
-    #Select a file for use based on hash
-    fileCount = len(glob.glob1("fractals","*.png"))
-    fracID = (imghash % fileCount) + 1
-    filename = "fractals" + CONFIG.SEPARATOR + str(fracID) + ".png"
-    #Read the file, resize it, then XOR
-    fractal = cv2.imread(filename, 1)
-    dim = img_in.shape
-    fractal = cv2.resize(fractal,(dim[0],dim[1]))
-    img_out = cv2.bitwise_xor(img_in,fractal)
-
-    return img_out
-
 mod = SourceModule("""
     #include <stdint.h>
     __global__ void ArCatMap(uint8_t *in, uint8_t *out)
@@ -139,7 +92,7 @@ def Encrypt():
         # Write intermediate files if debugging is enabled
         if CONFIG.DEBUG_CATMAP:
             imgAr = (np.reshape(imgAr_In,dim)).astype(np.uint8)
-            cv2.imwrite("catmap" + CONFIG.SEPARATOR + "iteration " + str(i) + ".png", imgAr)
+            cv2.imwrite("catmap\\iteration " + str(i) + ".png", imgAr)
     imgAr = (np.reshape(imgAr_In,dim)).astype(np.uint8)
     timer[2] = time.time() - timer[2]
     cv2.imwrite("3catmap.png", imgAr)
