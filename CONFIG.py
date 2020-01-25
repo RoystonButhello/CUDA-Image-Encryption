d8dd8a63da7f677000d497e0e2c617e60f1cf3cf
Saswat Levin <saswatlevin@gmail.com>
2020-01-25 20:16:17 +0530

    ADDED RNB's CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import platform
 # Note:- Use absolute path when executing via VSC, use relative path when executing via IDLE
@@ -29,22 +28,4 @@ AR_MIN_ITER = 50    #Min. catmap iterations
 AR_MAX_ITER = 150   #Max/ catmap iterations
 BUFF_SIZE = 65536   #Compute hash in 64kB chunks
 SOURCE = "raytracer480.png" #Input image
-=======
-'''Configure operation of the encryption algorithms via this file'''
-import os
 
-# Note:- Use absolute path when executing via VSC, use relative path when executing via IDLE
-PATH = os.path.dirname(os.path.abspath( __file__ )) + "\\"
-
-#Flags
-DEBUG_HISTEQ = False
-DEBUG_CATMAP = False
-DEBUG_TIMER = True
-
-#Constants
-MASK_BITS = 16
-AR_MIN_ITER = 100   #Min. catmap iterations
-AR_MAX_ITER = 110   #Max. catmap iterations
-BUFF_SIZE = 65536   #Compute hash in 64kB chunks
-SOURCE = "raytracer480.png" #Input image
->>>>>>> 04a45cd14c2feb74854b32b6a0c9d3087ee116b0
