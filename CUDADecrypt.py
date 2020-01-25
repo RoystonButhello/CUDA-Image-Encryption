bea975f2f11526bce14139833ece3e749ece4265
Saswat Levin <saswatlevin@gmail.com>
2020-01-25 20:08:55 +0530

    ADDED RNB's CUDADecrypt.py

--- CUDADecrypt.py
+++ CUDADecrypt.py
@@ -15,55 +15,6 @@ from pycuda.compiler import SourceModule
 
 os.chdir(CONFIG.PATH)
 
-
-def sha2Hash(filename):
-    hashobj = hashlib.sha256()
-    with open(filename,'rb') as f:
-        while True:
-            block = f.read(CONFIG.BUFF_SIZE)
-            if not block:
-                break
-            hashobj.update(block)
-    return int(hashobj.hexdigest(),16)
-
-# Mersenne-Twister Intra-Column-Shuffle
-def MTUnShuffle(img_in, imghash):
-    mask = 2**CONFIG.MASK_BITS - 1   # Default: 8 bits
-    temphash = imghash
-    dim = img_in.shape
-    N = dim[0]
-    img_out = img_in.copy()
-
-    for j in range(N):
-        random.seed(temphash & mask)
-        MTmap = list(range(N))
-        random.shuffle(MTmap)
-        temphash = temphash>>CONFIG.MASK_BITS
-        if temphash==0:
-            temphash = imghash
-        for i in range(N):
-            index = int(MTmap[i])
-            img_out[index][j] = img_in[i][j]
-    return img_out
-
-#XOR Image with a Fractal
-def FracXor(imghash):
-    #Open the image
-    img_in = cv2.imread("5imgfractal.png", 1)
-
-    #Select a file for use based on hash
-    fileCount = len(glob.glob1("fractals","*.png"))
-    fracID = (imghash % fileCount) + 1
-    filename = "fractals" + CONFIG.SEPARATOR + str(fracID) + ".png"
-    #Read the file, resize it, then XOR
-    fractal = cv2.imread(filename, 1)
-    dim = img_in.shape
-    fractal = cv2.resize(fractal,(dim[0],dim[1]))
-    img_out = cv2.bitwise_xor(img_in,fractal)
-
-    return img_out
-
-
 mod = SourceModule("""
     #include <stdint.h>
     __global__ void ArCatMap(uint8_t *in, uint8_t *out)
