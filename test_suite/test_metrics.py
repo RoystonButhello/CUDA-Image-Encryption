fc33d1dbab78a75972fc953fb085d4cfc041b885
Saswat Levin <saswatlevin@gmail.com>
2020-01-30 21:49:45 +0530

    Added correlationVert function

--- test_suite/test_metrics.py
+++ test_suite/test_metrics.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import math
+import time
 #module to collect metrics of different images
 
 
@@ -51,12 +52,34 @@ def correlationHoriz(img,N):
 			avg_corr_h=np.sum(corr_h_arr)/(len(corr_h_arr))
 	return avg_corr_h			
 
+#Returns the correlation of an image in the vertical direction
+def correlationVert(img,N):
+	corr_v_arr=np.zeros([N*N])
+	#print("\n length of corr_h_arr="+str(len(corr_h_arr)))
+	l=0
+	for i in range(0,N-1):
+		for j in range(0,N-1):
+			corr_v_arr[l]=correlation(img[j,i],img[j+1,i+1])
+			l=l+1
+			avg_corr_v=np.sum(corr_v_arr)/(len(corr_v_arr))
+	return avg_corr_v
+
+
 img_in =cv2.imread("8output.png",1)
 img_rgb = cv2.cvtColor(img_in, cv2.COLOR_BGR2RGB)
-img_rgb=cv2.resize(img_rgb,(480,480))
-dim=img_rgb.shape 
+#img_rgb=cv2.resize(img_rgb,(480,480))
+dim=img_in.shape 
 N=dim[0]
+time_array=np.zeros([2])
 
-avg_corr_h=correlationHoriz(img_rgb,N)
+st_1=time.time()
+avg_corr_h=correlationHoriz(img_in,N)
+time_array[0]=time.time()-st_1
+st_2=time.time()
+avg_corr_v=correlationVert(img_in,N)
+time_array[1]=time.time()-st_2
+print("\ntime for h correlation="+str(time_array[0]))
+print("\ntime for v correlation="+str(time_array[1]))
 #print(im_rgb[9,9])
 print("\navg_corr_h="+str(avg_corr_h))
+print("\navg_corr_v="+str(avg_corr_v))
