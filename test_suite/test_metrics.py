e74e12830abcb95149eff836e76cf7463a80bbb3
Saswat Levin <saswatlevin@gmail.com>
2020-02-01 00:13:49 +0530

    Added my_corel_horiz and my_corel_diag function

--- test_suite/test_metrics.py
+++ test_suite/test_metrics.py
@@ -2,121 +2,55 @@ import cv2
 import numpy as np
 import math
 import time
+
 #module to collect metrics of different images
-
-
-#Returns correlation coefficent of any vectors 
-def correlation(vector_x,vector_y):
-	N=len(vector_x)
-	i=0
-	#Covariance components
-	cv_x=0
-	cv_y=0
-	cov_x_y=0
-	sum_x=0
-	sum_y=0
-	
-	sum_x=np.sum(vector_x)
-	sum_y=np.sum(vector_y)	
-	
-	mean_x=sum_x/N;
-	mean_y=sum_y/N;
-	
-	for i in range(0,N):
-		cv_x=cv_x+vector_x[i]-mean_x
-		cv_y=cv_y+vector_y[i]-mean_y
-		#Covariance of x and y
-	
-	if cv_x==0 or cv_y==0:
-		corr_x_y=0
-		return corr_x_y
-
-	cov_x_y=(1/N)*cv_x*cv_y
-	var_x=(1/(N-1))*(cv_x*cv_x)
-	var_y=(1/(N-1))*(cv_y*cv_y)
-	sq_root_x=math.sqrt(var_x)
-	sq_root_y=math.sqrt(var_y)
-	sq_root_x_y=sq_root_x*sq_root_y
-	corr_x_y=cov_x_y/sq_root_x_y
-	return corr_x_y
-
-#Returns the average correlation of an image in horizontal direction
-def correlationHoriz(img,N):
-	corr_h_arr=np.zeros([N*N])
-	#print("\n length of corr_h_arr="+str(len(corr_h_arr)))
+#Returns the average horizontal auto correlation 
+def my_correl_horiz(img,N):
 	l=0
+	arr_dim=N-1
+	two_d_mat=np.zeros([2,2])
+	corr_array=np.zeros([arr_dim*arr_dim])
 	for i in range(0,N-1):
 		for j in range(0,N-1):
-			corr_h_arr[l]=correlation(img[i,j],img[i+1,j+1])
-			l=l+1
-			avg_corr_h=np.sum(corr_h_arr)/(len(corr_h_arr))
-	return avg_corr_h			
+			#if i==j:
+			two_d_mat=np.corrcoef(img[i,j],img[i+1,j+1])
+			if math.isnan(two_d_mat[0,1]):
+				corr_array[l]=0
+				l=l+1
+			else:
+				corr_array[l]=two_d_mat[0,1]
+				l=l+1
+	#print("\ncorrelation array=\n")
+	#print(corr_array)
+	return (np.sum(corr_array)/(len(corr_array)))
 
-#Returns the average correlation of an image in the vertical direction
-def correlationVert(img,N):
-	corr_v_arr=np.zeros([N*N])
-	#print("\n length of corr_h_arr="+str(len(corr_h_arr)))
+#Returns the average diagonal auto correlation 
+def my_correl_diag(img,N):
 	l=0
+	arr_dim=N-1
+	two_d_mat=np.zeros([2,2])
+	corr_array=np.zeros([arr_dim*arr_dim])
 	for i in range(0,N-1):
 		for j in range(0,N-1):
-			corr_v_arr[l]=correlation(img[j,i],img[j+1,i+1])
-			l=l+1
-			avg_corr_v=np.sum(corr_v_arr)/(len(corr_v_arr))
-	return avg_corr_v
+			if i==j:
+				two_d_mat=np.corrcoef(img[i,j],img[i+1,j+1])
+			if math.isnan(two_d_mat[0,1]):
+				corr_array[l]=0
+				l=l+1
+			else:
+				corr_array[l]=two_d_mat[0,1]
+				l=l+1
+	#print("\ncorrelation array=\n")
+	#print(corr_array)
+	return (np.sum(corr_array)/(len(corr_array)))	
 
-#Returns the average absolute difference in pixel color levels between plain and encrypted images
-#Still Testing MAE
-def MeanAbsoluteErr(img_plain,img_encrypted,M,N):
-	l=0
-	difference=np.zeros([M*N])
-	img_plain_sum=np.zeros([M*N])
-	img_encrypted_sum=np.zeros([M*N])
-	avg_mae=0
-	for i in range(0,M):
-		for j in range(0,N):	
-			img_plain_sum[l]=np.sum(img_plain[i,j])
-			img_encrypted_sum[l]=np.sum(img_encrypted[i,j])
-			difference[l]=(1/(M*N))*abs(img_plain_sum[l]-img_encrypted_sum[l])
-			l=l+1			
-	print("\nmae=\n")
-	print(difference)
-	avg_mae=np.sum(difference)/(len(difference))
-	return avg_mae			
-
-
-img_pln=cv2.imread("raytracer480.png",1)
-
-img_rgb_pln = cv2.cvtColor(img_pln, cv2.COLOR_BGR2RGB)
-img_rgb_pln=cv2.resize(img_rgb_pln,(10,10))
-dim_img_pln=img_rgb_pln.shape 
-N_img_pln=dim_img_pln[0]
-
-img_enc=cv2.imread("5imgfractal.png",1)
-img_rgb_enc=cv2.cvtColor(img_enc,cv2.COLOR_BGR2RGB)
-img_rgb_enc=cv2.resize(img_rgb_enc,(10,10))
-dim_img_rgb_enc=img_rgb_enc.shape
-N_img_enc=dim_img_rgb_enc[0]
-
-time_array=np.zeros([3])
-
-#st_1=time.time()
-#avg_corr_h=correlationHoriz(img_rgb_pln,N_img_pln)
-#time_array[0]=time.time()-st_1
-
-#st_2=time.time()
-#avg_corr_v=correlationVert(img_rgb_pln,N_img_pln)
-#time_array[1]=time.time()-st_2
-
-#print("\ntime for h correlation="+str(time_array[0]))
-#print("\ntime for v correlation="+str(time_array[1]))
-#print(im_rgb[9,9])
-
-#print("\navg_corr_h="+str(avg_corr_h))
-#print("\navg_corr_v="+str(avg_corr_v))
-
-st_3=time.time()
-avg_mae=MeanAbsoluteErr(img_rgb_pln,img_rgb_enc,N_img_pln,N_img_enc)
-time_array[2]=time.time()-st_3
-print("\navg_mae=\n")
-print(avg_mae)
-print("\ntime for mae="+str(time_array[2]))
+img_in=cv2.imread("raytracer480.png",1)
+img_pln=cv2.resize(img_in,(480,480))
+dim=img_pln.shape 
+N=dim[0]
+avg_corr_h=my_correl_horiz(img_pln,N)
+print("\navg_corr_horiz="+str(avg_corr_h))
+avg_corr_d=my_correl_diag(img_pln,N)
+print("\navg_corr_diag="+str(avg_corr_d))
+#print("\n")
+#print(np.corrcoef(img[i,j],img[i+1,j+1]))
