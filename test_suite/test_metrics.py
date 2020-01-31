52f76b6749a7d8c3ed843d5937fb8093e20c33f2
Saswat Levin <saswatlevin@gmail.com>
2020-02-01 00:39:59 +0530

    Added mean_absolute_err function

--- test_suite/test_metrics.py
+++ test_suite/test_metrics.py
@@ -44,13 +44,24 @@ def my_correl_diag(img,N):
 	#print(corr_array)
 	return (np.sum(corr_array)/(len(corr_array)))	
 
+def mean_absolute_err(img_p,img_e,N):
+	initial_mae = np.sum(np.absolute((img_p.astype(np.uint8)) - (img_e.astype(np.uint8))))
+	mae=initial_mae/(N*N)
+	print("\nmae="+str(mae))
+
+#Loading plain image
 img_in=cv2.imread("raytracer480.png",1)
 img_pln=cv2.resize(img_in,(480,480))
-dim=img_pln.shape 
-N=dim[0]
-avg_corr_h=my_correl_horiz(img_pln,N)
-print("\navg_corr_horiz="+str(avg_corr_h))
-avg_corr_d=my_correl_diag(img_pln,N)
-print("\navg_corr_diag="+str(avg_corr_d))
+dim_p=img_pln.shape 
+N=dim_p[0]
+	
+#Loading Encrypted image
+img_2=cv2.imread("8output.png",1)
+img_enc=cv2.resize(img_2,(480,480))
+#avg_corr_h=my_correl_horiz(img_pln,N)
+#print("\navg_corr_horiz="+str(avg_corr_h))
+#avg_corr_d=my_correl_diag(img_pln,N)
+#print("\navg_corr_diag="+str(avg_corr_d))
+mean_absolute_err(img_pln,img_enc,N)
 #print("\n")
 #print(np.corrcoef(img[i,j],img[i+1,j+1]))
