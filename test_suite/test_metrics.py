b1437119074798b1f1d73f5c799d37ecc445abb3
Saswat Levin <saswatlevin@gmail.com>
2020-02-01 15:08:19 +0530

    Corrected number_of_pixels_change_rate and uniform_average_changing_intensity

--- test_suite/test_metrics.py
+++ test_suite/test_metrics.py
@@ -61,7 +61,7 @@ def replace_pixel(img_p,row,col,green,blue,red):
 	img_p[row,col,0]=green
 	img_p[row,col,1]=blue
 	img_p[row,col,2]=red
-	cv2.imwrite("raytracer480_c.png",img_p)
+	cv2.imwrite("raytracer480_1_px_diff.png",img_p)
 
 #Returns the absolute difference between the pixel values of 2 encrypted images whose plain images are the same execept for a single pixel of difference
 def number_of_pixels_change_rate(img_e1,img_e2,N):
@@ -73,39 +73,55 @@ def number_of_pixels_change_rate(img_e1,img_e2,N):
 				if img_e1[i,j,k]!=img_e2[i,j,k]:
 					count=count+1
 	print("\ncount="+str(count))			
-	npcr=(count/(N*N))*100
+	npcr=((count/(N*N))*100)/3
 	return npcr
 
-#Returns the average absolute difference between the pixel values of 2 encrypted images whose plain images are the same except for a single pixel of difference
+#Returns the  absolute difference between the pixel values of 2 encrypted images whose plain images are the same except for a single pixel of difference
 def unified_average_changing_intensity(img_e1,img_e2,N):
-	initial_uaci = np.sum(np.absolute((img_e1.astype(np.uint8)) - (img_e2.astype(np.uint8))))
-	final_uaci=(initial_uaci*100)/(N*N*255)
-	print("\nfinal_uaci="+str(final_uaci))
-	#return avg_uaci			
+	uaci=0
+	for i in range(0,N):
+		for j in range(0,N):
+			for k in range(0,3):
+				uaci_calc=(np.absolute(img_e1[i,j,k]-img_e2[i,j,k]))/(N*N*255)
+				uaci=uaci+uaci_calc
+	uaci=(uaci*100)/3			
+	return uaci			
 
-#Loading plain image
-img_in=cv2.imread("5imgfractal.png",1)
-img_e1=cv2.resize(img_in,(10,10))
+img_1=cv2.imread("5imgfractal.png",1)
+img_e1=cv2.resize(img_1,(1024,1024))
 dim=img_e1.shape 
 N=dim[0]
 	
 #Loading Encrypted image
-img_2=cv2.imread("5imgfractal_c.png",1)
-img_e2=cv2.resize(img_2,(10,10))
+img_2=cv2.imread("5imgfractal_1px_diff.png",1)
+img_e2=cv2.resize(img_2,(1024,1024))
+
+#Loading plain image
+#img_3=cv2.imread("raytracer480.png",1)
+#img_pln=cv2.resize(img_3,(480,480))
+
 
 #avg_corr_h=my_correl_horiz(img_e1,N)
 #print("\navg_corr_horiz="+str(avg_corr_h))
 #avg_corr_d=my_correl_diag(img_pln,N)
 #print("\navg_corr_diag="+str(avg_corr_d))
 
-#avg_mae=mean_absolute_err(img_pln,img_enc,N)
-#print("\navg_mae="+str(avg_mae))
+#mae=mean_absolute_err(img_pln,img_e1,N)
+#print("\navg_mae="+str(mae))
 
-#replace_pixel(img_in,0,1,0,0,0)
+#replace_pixel(img_pln,0,2,10,33,49)
 
 npcr=number_of_pixels_change_rate(img_e1,img_e2,N)
 print("\nnpcr="+str(npcr))
-unified_average_changing_intensity(img_e1,img_e2,N)
+uaci=unified_average_changing_intensity(img_e1,img_e2,N)
+print("\nuaci="+str(uaci))
 #print("\navg_uaci="+str(avg_uaci))
 #print("\n")
 #print(np.corrcoef(img[i,j],img[i+1,j+1]))
+
+"""Correlation horiz=0.67...
+   Correlation_vertical=0.67
+   
+   #between img_enc and img_pln
+   MAE=271"""
+	
