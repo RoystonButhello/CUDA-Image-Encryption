e772eb2284807f2e2d019b982e2237436ff7fbcc
Saswat Levin <saswatlevin@gmail.com>
2020-02-26 00:59:56 +0530

    Rearranged genRelocVec

--- src/functions.hpp
+++ src/functions.hpp
@@ -4,6 +4,7 @@
   
   #include <iostream> /*For IO*/
   #include <functional>
+  #include <ctime>    /*For time*/
   #include <cstdint>  /*For standardized data types*/
   #include <cstdio>   /*For printf*/
   #include <random>   /*For random number generation*/
@@ -16,14 +17,11 @@
   #include <bitset>   /*For to_string()*/
   #include <opencv2/opencv.hpp> /*For OpenCV*/
   #include <opencv2/highgui/highgui.hpp>
-  
-  #define RAND_UPPER 32
-  #define EXP 100000000000000   
-  #define UNZERO 0.0000001
-  #define NUM_SKIPOFFSET_ARGS 2
-  #define WRITE_SINGLE_ARG 0
-  #define WRITE_MULTIPLE_ARGS 1
-  #define SEED1 30
+
+  #define RAND_UPPER 32   
+  #define WRITE_ALPHA_BETA 0
+  #define WRITE_OTHER_CONSTANTS 1
+  #define SEED1 10
   #define SEED2 32
  
   #define RESIZE_TO_DEBUG 1
@@ -32,34 +30,15 @@
   #define DEBUG_IMAGES    0
   
   using namespace std;
-  using namespace cv;   
-  
-     struct constants
-     {
-      uint8_t alpha;
-      uint8_t beta;
-      uint8_t a;
-      uint8_t b;
-      uint16_t c;
-      float x;
-      float y;
-      int32_t r;
-      uint8_t offset;
-     };
-    
-  /*Function Prototypes*/
-  
-  constants initializeConstants(uint32_t,uint32_t,uint32_t); 
-  void writeToFile(std::string,uint8_t,uint8_t,uint8_t,uint8_t,uint16_t,float,float,int32_t,uint8_t,bool);   
-  std::vector<float> skipOffset(uint8_t,uint8_t,uint16_t,float,float,float,uint8_t);
-  void generateP(std::vector<float> &P,uint8_t,uint8_t,uint16_t,float,float,float,uint32_t,uint32_t);
-  void generateU(std::vector<uint32_t> &U,std::vector<float>,int32_t,uint32_t,uint32_t);   
-  std::vector<uint8_t> flattenGrayScaleImage(cv::Mat image);
-  void printImageContents(cv::Mat image);
-  std::vector<int32_t> readFromFile(std::string,std::vector<int32_t>);
-  bool checkDecimal(std::vector<float>,uint32_t);
-  
+  using namespace cv;
 
+  /*Function Prototypes*/
+  void flattenGrayScaleImage(cv::Mat image,std::vector<uint8_t> &img_vec);
+  void printImageContents(cv::Mat image);
+  bool checkDecimal(std::vector<float> arr,uint32_t total);
+  void genRelocVec(std::vector<uint8_t> &U,std::vector<float> &P,uint16_t m,uint16_t n,uint8_t seed);
+  void printFloatVector(std::vector<float> vect);
+  void printInt8Vector(std::vector<uint8_t> vect);
     
 
   bool checkDecimal(std::vector<float> arr,uint32_t TOTAL)
@@ -75,6 +54,22 @@
     return 0;
   }
   
+ void printFloatVector(std::vector<float> vect)
+ {
+  for(uint32_t i=0;i<vect.size();++i )
+  {
+   printf("%f ",vect[i]);
+  }
+ }
+  
+ void printInt8Vector(std::vector<uint8_t> vect)
+ {
+   for(uint32_t i=0;i<vect.size();++i )
+  {
+   printf("%d ",vect[i]);
+  }
+ }
+  
   void printImageContents(cv::Mat image)
   {
     cout<<"\nImage Matrix=";
@@ -88,179 +83,85 @@
       }
     }
   }  
-  
-  std::vector<float> skipOffset(uint8_t a,uint8_t b,uint16_t c,float x,float y,float unzero,uint8_t offset)
-  {
-    printf("\nIn skipOffset\n");
-    std::vector<float> result(NUM_SKIPOFFSET_ARGS);
-    std::vector<uint8_t> loop_range(offset);
-    // Fill loop_range with 0, 1, ..., offset-1
-    std::iota (std::begin(loop_range), std::end(loop_range), 0);
-   printf("\nBefore Offset loop"); 
-   for (uint8_t i : loop_range)
-   {    
-	x = fmod((x + a*y),1.0) + unzero; 
-   
-        y = fmod((b*x + c*y),1.0) + unzero;
-        
-        //printf("\ni=%d\t",i);
-   }
-   
-   //printf("\noffset=%d\t",offset);
-   //printf("\nx=%f",x);
-   //printf("\ny=%f",y);
-   
-   result[0]=x;
-   result[1]=y;
-   return result; 
-  } 
-
-  constants initializeConstants(uint32_t seed,uint32_t offset,uint32_t R_UPPER)
-  {
-    printf("\nIn InitializeCOnstants\n");
-    struct constants constants_array;
-    
-    //uint32_t seed;
-
-    /*Seeding the rand() generator*/
-    srand(seed+offset);
-
-    /*Assigning random values to constants*/
-    //constants_array.push_back(constants());
-    constants_array.alpha =  RAND_UPPER + rand() / (RAND_MAX / ( 1 - RAND_UPPER + 1) + 1); 
-    constants_array.beta  =  RAND_UPPER + rand() / (RAND_MAX / (1 - RAND_UPPER + 1) + 1);
-    constants_array.a =      RAND_UPPER + rand() / (RAND_MAX / (2 - RAND_UPPER + 1) + 1);
-    constants_array.b =      RAND_UPPER + rand() / (RAND_MAX / (2 - RAND_UPPER + 1) + 1);
-    constants_array.c =      1 + (constants_array.a * constants_array.b);
-    constants_array.x =      0 + rand() / ( RAND_MAX / (0.0001 - 1.0 + 1.0) + 1.0);
-    constants_array.y =      0 + rand() / ( RAND_MAX / (0.0001 - 1.0 + 1.0) + 1.0);
-    constants_array.r =      1+(rand()%RAND_UPPER);
-    constants_array.offset = RAND_UPPER + rand() / (RAND_MAX / ( 1 - RAND_UPPER + 1) + 1);
-
-    
-    return constants_array;
-  }
-  
-void writeToFile(std::string filename,uint8_t alpha,uint8_t beta,uint8_t a,uint8_t b,uint16_t c,float x,float y,int32_t r,uint8_t offset,bool mode)
-  { 
-    printf("\nIn WriteToFile\n");
-    //std::string stringToWrite=std::string("");
-    std::string constant=std::string("");
-    
-    /*if (mode==1)
-    {
-     constant=std::to_string(alpha);
-     stringToWrite+=constant;
-     stringToWrite+="\n";
-     return;
-    }*/
-    
-    
-    constant.append(std::to_string(alpha));
-    constant.append("\n");
-    
-    constant.append(std::to_string(beta));
-    constant.append("\n");
-    
-    constant.append(std::to_string(a));
-    constant.append("\n");
-    
-    constant.append(std::to_string(b));
-    constant.append("\n");
-    
-    constant.append(std::to_string(c));
-    constant.append("\n");
-    
-    constant.append(std::to_string(x));
-    constant.append("\n");
-  
-    constant.append(std::to_string(y));
-    constant.append("\n");
-    
-    constant.append(std::to_string(offset));
-    constant.append("\n");
-
-  
-    //cout<<"\nstringToWrite= "<<stringToWrite;
-    /*Output stream to write to file*/
-    //cout<<"\nCONSTS=";
-    //cout<<constant;
-    std::ofstream file(filename);
-    file << constant;
-    file.close();
-    
-}  
-  
-  std::vector<int32_t> readFromFile(std::string filename,std::vector<int32_t> retrieved_constants_array)
-  {
-    int32_t constant; 
-    uint8_t i=0;
-    std::fstream inFile;
-    inFile.open(filename,ios::in);
-    if(!inFile)
-    {
-      cout<<"\nFile Not Found\nExiting...";
-      exit(0);
-    }
-    
-    while (inFile >> constant)
-    {
-       retrieved_constants_array.insert(retrieved_constants_array.begin()+i,constant);
-       cout<<"\n"<<retrieved_constants_array[i];
-       ++i;
-    }
-    inFile.close();
-    return retrieved_constants_array;  
-  }
-  
-  void generateP(std::vector<float> &P,uint8_t a,uint8_t b,uint16_t c,float x,float y,float unzero,uint32_t MID,uint32_t TOTAL)
-  {  
-    
-    //std::vector<float> P(TOTAL);
-    printf("\nIn GenerateP\n");
-    //float intermediate_x=0,intermediate_y=0;
-    printf("\nBefore GenP loop\n");
-    for(uint32_t i=0;i<MID;++i)
-    { 
-      //printf("\ni=%d\t",i);
-      x=fmod((x+a*y),1.0)+unzero;
-      y=fmod((b*x+c*y),1.0)+unzero;
-      
-      P[2*i]=x;
-      P[2*i+1]=y;
-    }    
-    
-    //return P;
-  } 
-    
-
-  void generateU(std::vector<uint32_t> &U,std::vector<float> P,int32_t r,uint32_t M,uint32_t N)
-  {
-    printf("\nIn genU\n");
-    //float dN=(float)N;
-    float remainder;
-    //std::vector<uint32_t> U(M);
-    //cout<<"\nBefore entering the genU loop\n";
-    printf("\nBeforeGenU Loop\n");
-    for(uint16_t i=0;i<M;++i)
-    {
-      
-      //printf("\ni=%d\t",i); 
-      remainder=fmod((P[r+i]*EXP),N); 
-      //printf("\n%f",remainder);
-      U[i]=remainder;
-    }
-   
-   //return U; 
- }   
-     
- std::vector<uint8_t> flattenGrayScaleImage(cv::Mat image)
+ 
+ void flattenGrayScaleImage(cv::Mat image,std::vector<uint8_t> &img_vec)
  {
-    //std::vector<uint8_t> img_vec(M*N);
+   ;
     
-    std::vector<uint8_t> img_vec((uint8_t*)image.data, (uint8_t*)image.data + image.rows * image.cols);
-    
-    return img_vec;
  }
 
+ 
+ void genRelocVec(std::vector<uint8_t> &U,std::vector<float> &P,std::string filename,int16_t m,uint16_t n,uint8_t seed)
+ {
+   /*Initialize PRNGs*/
+   srand(seed);
+   double unzero=0.0000001;
+   uint16_t total=(m*n);
+   uint16_t mid=((total)/2);
+   
+   if(m%2!=0)
+   {
+     mid=mid+1;
+   }
+   
+   uint64_t exponent=100000000000000;
+   
+   std::string parameters=std::string("");
+   
+   /*Initialize random parameters*/
+   uint8_t a=2+(rand()%32);
+   uint8_t b=2+(rand()%32);
+   uint16_t c=1+(a*b);
+   uint8_t  offset=1+(rand()%32);
+   double x=0 + rand() / ( RAND_MAX / (0.0001 - 1.0 + 1.0) + 1.0);
+   double y=0 + rand() / ( RAND_MAX / (0.0001 - 1.0 + 1.0) + 1.0);
+   
+   /*Converting parameters to string and writing to file*/
+   parameters.append(std::to_string(a));
+   parameters.append("\n");
+   parameters.append(std::to_string(b));
+   parameters.append("\n");
+   parameters.append(std::to_string(c));
+   parameters.append("\n");
+   parameters.append(to_string(offset));
+   parameters.append("\n");
+   
+   std::ofstream file(filename);
+   
+   if(!file)
+   {
+     cout<<"\nCouldn't write parametes to file.\nExiting...";
+     exit(0);
+   }
+   
+   file<<parameters;
+   file.close();
+   
+   /*Skip offset values*/
+   for(uint32_t i=0;i<offset;++i)
+   {
+     x = fmod((x + a*y),1) + unzero;
+     y = fmod((b*x + c*y),1) + unzero;
+   
+   }
+   
+   /*generate P vector*/
+   for(uint32_t i=0;i<mid;++i)
+   {
+     x = fmod((x + a*y),1) + unzero;
+     y = fmod((b*x + c*y),1) + unzero;
+     P[2*i]=x;
+     P[2*i+1]=y;
+     
+   }
+   
+   /*Generate U Vector*/
+   for(uint32_t i=0;i<m;++i)
+   {
+    U[i]=fmod((P[i]*exponent),m);
+   }
+   
+   
+ } 
+ 
 #endif
