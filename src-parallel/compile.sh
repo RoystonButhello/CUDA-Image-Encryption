56f1a33cd0b495f87870de192c5bf4074c561a20
Saswat K. Levin <saswatlevin@gmail.com>
2020-05-31 12:21:44 +0530

    Added -O3 level of optimization to compile.sh

--- src-parallel/compile.sh
+++ src-parallel/compile.sh
@@ -1,2 +1,2 @@
-nvcc -std=c++11 kernel.cu main.cpp -o main `pkg-config opencv --cflags --libs` -I/opt/ssl/include/ -L/opt/ssl/lib/ -lcrypto
-nvcc -std=c++11 kernel.cu decrypt.cpp -o decrypt `pkg-config opencv --cflags --libs` -I/opt/ssl/include/ -L/opt/ssl/lib/ -lcrypto
+nvcc -O3 -std=c++11 kernel.cu main.cpp -o main `pkg-config opencv --cflags --libs` -I/opt/ssl/include/ -L/opt/ssl/lib/ -lcrypto
+nvcc -O3 -std=c++11 kernel.cu decrypt.cpp -o decrypt `pkg-config opencv --cflags --libs` -I/opt/ssl/include/ -L/opt/ssl/lib/ -lcrypto
